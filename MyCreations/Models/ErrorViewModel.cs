c62e3e54ce27ada60db589dc224c4d9f2c216b52
Anubhav Gupta <anubhavgupta.gupta28@gmail.com>
2020-07-19 15:18:44 +0530

    initial commit

---
+++ MyCreations/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyCreations.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyCreations/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCreations.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string OwnerName { get; set; }
+    }
+}
